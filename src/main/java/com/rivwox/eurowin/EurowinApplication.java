7e7259b4918c07d6d9248887b597f7119f7849fc
Augusto Rivera <rivera_augusto@protonmail.com>
2021-05-15 19:27:06 -0500

    Initial Commit

---
+++ src/main/java/com/rivwox/eurowin/EurowinApplication.java
@@ -0,0 +1,13 @@
+package com.rivwox.eurowin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurowinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurowinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rivwox/eurowin/EurowinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivwox.eurowin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurowinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
